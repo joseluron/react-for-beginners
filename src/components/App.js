34c5b3a13428b183f3436ed87728fb98ee1f6097
luron <joseluron@gmail.com>
2018-06-16 21:12:23 +0200

    Update order state

--- src/components/App.js
+++ src/components/App.js
@@ -22,12 +22,25 @@ class App extends React.Component {
         fishes[`fish${Date.now()}`] = fish;
         // 3. Set the new fishes object to state
         this.setState({
-            fishes: fishes
+            // fishes: fishes
+            fishes
         });
     }
 
     loadSampleFishes = () => {
         this.setState({ fishes: sampleFishes });
+    }
+
+    addToOrder = (key) => {
+        // 1. Take a copy of state
+        const order = {...this.state.order};
+        // 2. Either add to the order, or update the number in our order
+        order[key] = order[key] + 1 || 1;
+        // 3. Call setState to update our state object
+        this.setState({
+            // order: order
+            order
+        });
     }
 
     render() {
@@ -37,7 +50,11 @@ class App extends React.Component {
                     <Header tagline="Fresh Seafood Market" />
                     <ul className="fishes">
                         { Object.keys(this.state.fishes)
-                            .map(key => <Fish key={key} details={this.state.fishes[key]} />) }
+                            .map(key => <Fish 
+                                        key={key}
+                                        index={key} 
+                                        details={this.state.fishes[key]} 
+                                        addToOrder={this.addToOrder} />) }
                     </ul>
                 </div>
                 <Order />
